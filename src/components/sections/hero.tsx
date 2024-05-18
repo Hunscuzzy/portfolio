ba3be827db0dfeed36fa11291632ca4c1cdf89da
Hugo Fontvieille <32163579+Hunscuzzy@users.noreply.github.com>
2024-05-18 11:22:38 +0200

    feat(various): some improvments about design

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -10,63 +10,65 @@ export default function Hero() {
   }));
   return (
     <section
-      id='🏠'
-      className='group bg-gradient-to-tr dark:from-black dark:to-gray-900 from-indigo-100 to-blue-200 min-h-screen flex items-center py-16 px-4'
+      id="🏠"
+      className="group bg-gradient-to-tr dark:from-black dark:to-gray-900 from-indigo-100 to-blue-200 min-h-screen flex items-center py-16 px-4"
       onMouseMove={({ clientX: x, clientY: y }) =>
         set({ xy: [x - window.innerWidth / 2, y - window.innerHeight / 2] })
       }
     >
-      <div className='container mx-auto flex justify-center items-center'>
+      <div className="container mx-auto flex justify-center items-center">
         <animated.div
           style={{
             transform: props.xy.to(
-              (x, y) => `translate3d(${x / 15}px, ${y / 15}px, 0)`
+              (x, y) => `translate3d(${x / 15}px, ${y / 15}px, 0)`,
             ),
           }}
         >
           <Image
-            src='/images/hugo-fontvieille.png'
-            alt='Hugo Fontvieille'
+            src="/images/hugo-fontvieille.png"
+            alt="Hugo Fontvieille"
             width={250}
             height={600}
-            className='z-0 blur-sm brightness-200 filter group-hover:blur-none transition-all delay-250 duration-500'
+            className="z-0 blur-sm brightness-200 filter group-hover:blur-none transition-all delay-250 duration-500"
           />
         </animated.div>
-        <div className='mt-32 absolute flex items-center gap-8 flex-col md:flex-row-reverse drop-shadow'>
+        <div className="mt-32 absolute flex items-center gap-8 flex-col md:flex-row-reverse drop-shadow">
           <animated.div
             style={{
               transform: props.xy.to(
-                (x, y) => `translate3d(${x / 15}px, ${y / 15}px, 0)`
+                (x, y) => `translate3d(${x / 15}px, ${y / 15}px, 0)`,
               ),
             }}
           >
-            <h1 className='section-title'>
+            <h1 className="section-title">
               Hugo
               <br /> Fontvieille
             </h1>
-            <i className='font-normal text-md'>/fɔ̃.vjɛj/</i>
+            <i className="font-normal text-md">/fɔ̃.vjɛj/</i>
           </animated.div>
-          <div className='text-center md:text-right text-xl leading-normal'>
+          <div className="text-center md:text-right text-xl leading-normal">
             <h2>
               French <i>Freelance & Entrepreneur</i>, <br /> Working as{" "}
-              <i>Front-end developer</i> , <br /> Living in <i>Alicante</i>,
-              Spain.
+              <i>Front-end developer</i> , <br /> Living in{" "}
+              <i className="blur-sm">Alicante, Spain.</i>
+              <br />
+              <span className="text-2xl">Available Worldwide remotely 🌎</span>.
             </h2>
-            <a className='underline italic text-sm mt-2 block' href='/#👤'>
+            <a className="underline italic text-sm mt-2 block" href="/#👤">
               Know more
             </a>
-            <div className='flex justify-end mt-4 gap-x-2 text-xl'>
+            <div className="flex justify-end mt-4 gap-x-2 text-xl">
               <a
-                href='https://fr.linkedin.com/in/hugo-fontvieille'
-                target='_blank'
-                className='hover:text-blue-700 transition'
+                href="https://fr.linkedin.com/in/hugo-fontvieille"
+                target="_blank"
+                className="hover:text-blue-700 transition"
               >
                 <IoLogoLinkedin />
               </a>
               <a
-                href='https://github.com/Hunscuzzy'
-                target='_blank'
-                className='hover:text-blue-700 transition'
+                href="https://github.com/Hunscuzzy"
+                target="_blank"
+                className="hover:text-blue-700 transition"
               >
                 <IoLogoGithub />
               </a>
@@ -74,7 +76,7 @@ export default function Hero() {
           </div>
         </div>
         <button
-          className='absolute bottom-4 text-4xl animate-bounce hover:text-blue-700 transition'
+          className="absolute bottom-4 text-4xl animate-bounce hover:text-blue-700 transition"
           onClick={() => {
             document?.querySelector("#👤")?.scrollIntoView({
               behavior: "smooth",
--- src/components/sections/works.tsx
+++ src/components/sections/works.tsx
@@ -20,19 +20,19 @@ import {
 export default function Works() {
   return (
     <section
-      id='💼'
-      className='relative bg-gradient-to-l dark:from-gray-950 dark:to-indigo-950 from-gray-50 to-indigo-300  min-h-screen flex items-center py-16 px-4'
+      id="💼"
+      className="relative bg-gradient-to-l dark:from-gray-950 dark:to-indigo-950 from-gray-50 to-indigo-300  min-h-screen flex items-center py-16 px-4"
     >
-      <div className='bg-carbon opacity-20 absolute w-full h-full left-0 top-0' />
+      <div className="bg-carbon opacity-20 absolute w-full h-full left-0 top-0" />
 
-      <div className='relative container mx-auto flex flex-col md:flex-row-reverse justify-between gap-16'>
-        <div className='md:sticky top-8 h-fit md:text-right'>
-          <h2 className='section-title'>
-            what <br className='hidden sm:block' /> I{" "}
-            <br className='hidden sm:block' /> did
+      <div className="relative container mx-auto flex flex-col md:flex-row-reverse justify-between gap-16">
+        <div className="md:sticky top-8 h-fit md:text-right">
+          <h2 className="section-title">
+            what <br className="hidden sm:block" /> I{" "}
+            <br className="hidden sm:block" /> did
           </h2>
-          <p className='text-xs mb-2'>(my day-to-day stack)</p>
-          <div className='flex md:justify-end gap-2 text-lg flex-wrap'>
+          <p className="text-xs mb-2">(my day-to-day stack)</p>
+          <div className="flex md:justify-end gap-2 text-lg flex-wrap max-w-32">
             <SiTypescript />
             <SiJest />
             <SiCypress />
@@ -45,41 +45,40 @@ export default function Works() {
             <SiGatsby />
             <SiSwift />
             <SiCss3 />
-            pp
           </div>
         </div>
-        <div className='relative border-s border-gray-200 dark:border-gray-700 space-y-4'>
+        <div className="relative border-s border-gray-200 dark:border-gray-700 space-y-4">
           {works.map((work) => (
             <div key={work.title}>
               <Disclosure>
                 {({ open }) => (
                   <>
-                    <Disclosure.Button className='group text-left w-full'>
-                      <span className='absolute flex tems-center justify-center w-6 h-6 bg-white group-hover:bg-slate-200 rounded-full -start-3 ring-7 ring-white dark:ring-blue-900 dark:bg-blue-900  dark:group-hover:bg-blue-700'>
+                    <Disclosure.Button className="group text-left w-full">
+                      <span className="absolute flex tems-center justify-center w-6 h-6 bg-white group-hover:bg-slate-200 rounded-full -start-3 ring-7 ring-white dark:ring-blue-900 dark:bg-blue-900  dark:group-hover:bg-blue-700">
                         {open ? (
-                          <IoRemove className='w-4 h-4 text-blue-800 dark:text-blue-300' />
+                          <IoRemove className="mt-1 w-4 h-4 text-blue-800 dark:text-blue-300" />
                         ) : (
-                          <IoAdd className='w-4 h-4 text-blue-800 dark:text-blue-300' />
+                          <IoAdd className="mt-1 w-4 h-4 text-blue-800 dark:text-blue-300" />
                         )}
                       </span>
-                      <h3 className='ml-8 flex items-center mb-1 text-xl font-medium text-blue-950 dark:text-white'>
+                      <h3 className="ml-8 flex items-center mb-1 text-xl font-medium text-blue-950 dark:text-white">
                         {work.title}
                       </h3>
                     </Disclosure.Button>
-                    <time className='ml-8 mb-2 text-sm font-normal leading-none text-blue-800 dark:text-blue-300'>
+                    <time className="ml-8 mb-2 text-sm font-normal leading-none text-blue-800 dark:text-blue-300">
                       {work.date}
                     </time>
                     <Transition
-                      className='transition transform origin-left'
-                      enter='duration-200 ease-out'
-                      enterFrom='scale-90 opacity-0'
-                      enterTo='scale-100 opacity-100'
-                      leave='duration-100 ease-out'
-                      leaveFrom='scale-100 opacity-100'
-                      leaveTo='scale-90 opacity-0'
+                      className="transition transform origin-left"
+                      enter="duration-200 ease-out"
+                      enterFrom="scale-90 opacity-0"
+                      enterTo="scale-100 opacity-100"
+                      leave="duration-100 ease-out"
+                      leaveFrom="scale-100 opacity-100"
+                      leaveTo="scale-90 opacity-0"
                     >
-                      <Disclosure.Panel className='ml-8'>
-                        <p className='mb-4 text-base font-normal text-gray-800 dark:text-gray-200'>
+                      <Disclosure.Panel className="ml-8">
+                        <p className="mb-4 text-base font-normal text-gray-800 dark:text-gray-200">
                           {work.content}
                         </p>
                       </Disclosure.Panel>
